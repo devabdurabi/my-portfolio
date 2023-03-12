bd5bd73ba04a8fffe32f759be2e069479630e373
Abdurabi <amabukar070@gmail.com>
2023-03-12 22:36:32 +0100

    navbar created for now

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,13 +1,105 @@
-import React from 'react'
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <section className='fixed w-full h-20 shadow-xl z-[100]'>
-        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+    <section className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image src="/../public/assets/logosa.png" alt="/" width="60" height="60" />
 
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm hover:border-b">Projects</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm hover:border-b">Contact</li>
+            </Link>
+          </ul>
+          <div onClick={handleNav} className="md:hidden">
+            <AiOutlineMenu size={27} />
+          </div>
         </div>
-    </section>
-  )
-}
+      </div>
 
-export default Navbar
+      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+        <div
+          className={
+            nav
+              ? 'md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+          }
+        >
+          <div className="flex w-full items-center justify-between">
+            <Image src="/../public/assets/logosa.png" alt="/" width="75" height="75" />
+            <div
+              onClick={handleNav}
+              className="rounded-lg shadow-lg shadow-gray-500 p-1 cursor-pointer"
+            >
+              <AiOutlineClose size={26} />
+            </div>
+          </div>
+
+          <div className="border-b border-gray-300 my-12">
+            <p className="w-[85%] md:w-[90%] py-4">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente repudiandae impedit
+              placeat vel culpa.{' '}
+            </p>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul>
+              <Link href="/">
+                <li className="py-4 text-sm">Home</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href="/">
+                <li className="py-4 text-sm">Contact</li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase -tracking-widest text-[#5651e5]">Laten we werken</p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-100 ease-in duration-200">
+                  <FaLinkedinIn />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-100 ease-in duration-200">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-100 ease-in duration-200">
+                  <BsFillPersonLinesFill />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-100 ease-in duration-200">
+                  <AiOutlineMail />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
+// export default dynamic (() => Promise.resolve(Navbar), {ssr:true});
--- app/page.jsx
+++ app/page.jsx
@@ -7,10 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <section>
+    <div>
       
       
-    </section>
+    </div>
      
 
   )
